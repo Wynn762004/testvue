65af4089e0e11af0dce9e283c0b21429de183455
jxyler <zazaopama777@gmail.com>
2024-09-04 11:54:07 +0700

    my first commit

---
+++ my-app/src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+/*import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'*/
+import navbar from './components/navbar.vue'
+import card from './components/card.vue'
+import items from './test.json'
+</script>
+
+<template>
+<div>
+  <header>
+    
+    <navbar />
+    
+    <!--<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div> -->
+  </header>
+
+  <main>
+    <div class="grid-container">
+      <card v-bind="items[0]" />
+      <card v-bind="items[1]" />
+      <card v-bind="items[2]" />
+      <card v-bind="items[3]" />
+      <card v-bind="items[4]" />
+      <card v-bind="items[5]" />
+      <card v-bind="items[6]" />
+      <card v-bind="items[7]" />
+      <card v-bind="items[8]" />
+      <card v-bind="items[9]" />
+      <card v-bind="items[10]" />
+      <card v-bind="items[11]" />
+    </div>
+    <!-- <card /> -->
+
+  </main>
+</div>
+</template>
+
+<style scoped>
+/*header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}*/
+</style>
---
+++ my-app/src/components/card.vue
@@ -0,0 +1,31 @@
+<template lang="">
+  <div class="card">
+    <img :src="image" alt="name" style="width:100%">
+    <div class="container">
+      <h4><b>{{ name }}</b></h4>
+      <p>{{ nickname }}</p>
+      <p>{{ tel }}</p>
+      <p>{{ email}}</p>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['id','name','nickname','tel','email','image']
+}
+</script>
+<style scoped>
+    .card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 70%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
